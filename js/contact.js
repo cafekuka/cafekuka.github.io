1b118c21ff10f06bcde8c610f5f9cff229e6fdc0
cinsti621 <cinsti621@gmail.com>
2023-01-21 20:15:02 +0000

    variable fix

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,7 @@
 let content = document.getElementById("content")
 
 content.oninput = function() {
-    textarea.style.height = "";
+    content.style.height = "";
     /* textarea.style.height = Math.min(textarea.scrollHeight, 300) + "px"; */
-  textarea.style.height = textarea.scrollHeight + "px"
+    content.style.height = content.scrollHeight + "px"
   };
