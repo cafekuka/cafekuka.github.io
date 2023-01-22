fdefd0949afff5a5db241bc185d73c340c1c9e57
cinsti621 <cinsti621@gmail.com>
2023-01-22 00:23:25 +0000

    Summary

--- js/cafekuka.js
+++ js/cafekuka.js
@@ -1,3 +1,10 @@
+let creds = localStorage.getItem("creds")
+if (!creds) {
+    creds = JSON.stringify({uuid: Math.random().toString(16).slice(2), ts: +new Date()})
+    localStorage.setItem("creds", creds)
+}
+creds = JSON.parse(creds);
+
 $(function(){
     $("#navbar").load("../html/navbar.html", function() {
         $("#navbar").find('[data-tab=' + $("#navbar").data("active")+ ']').addClass("active")
